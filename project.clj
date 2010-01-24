42d60bb7fa4a426747ca64fdbb73b9913842390c
Daniel Renfer <drenfer@macbook.local>
2010-01-24 16:28:16 -0500

    add project.clj and remove dependencies

---
+++ project.clj
@@ -0,0 +1,13 @@
+(defproject clj-mql "1.0.0-SNAPSHOT"
+  :description "MQL client library for Clojure."
+  :url "http://github.com/rnewman/clj-mql"
+  :dependencies [[org.clojure/clojure "1.1.0"]
+                 [org.clojure/clojure-contrib "1.1.0-master-SNAPSHOT"]
+                 [org.apache.james/apache-mime4j "0.6"]
+                 [com.twinql.clojure/clj-apache-http "1.1.0"]
+                 [org.clojars.liebke/clojure-new-json "1.1-SNAPSHOT"]
+                 [commons-codec/commons-codec "1.4"]
+                 [commons-logging/commons-logging "1.1.1"]
+                 [org.apache.httpcomponents/httpclient "4.0.1"]
+                 [org.apache.httpcomponents/httpcore "4.0.1"]
+                 [org.apache.httpcomponents/httpmime "4.0.1"]])
